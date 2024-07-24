daf33fce83cdccc520e9c8c476ed5ff48f299ab1
whis102 <chicongnguyen.dev@gmail.com>
2024-07-24 19:27:32 +0700

    first commit

---
+++ Buổi 1/main.py
@@ -0,0 +1,14 @@
+# print
+'''
+print(5 + 10)
+print("I am", 15, "years old")
+'''
+
+# Input
+'''
+name = input("Enter name: ")
+print("Hello, welcome", name, "to my class")
+'''
+
+for i in range(1, 11):
+    print(i)
