9c119475df9b625085cf64621d1a092f900f2900
whis102 <chicongnguyen.dev@gmail.com>
2024-07-24 20:11:18 +0700

    update

---
+++ Buổi 3/input.py
@@ -0,0 +1,5 @@
+a = int(input("Nhập vào số a: "))
+b = int(input("Nhập vào số b: "))
+c = a + b
+
+print(c)
---
+++ Buổi 3/main.py
@@ -0,0 +1,10 @@
+name = "Nguyen Van A" #String
+age = 9.6 #float
+age1 = 9 #int
+
+a = []
+
+print(type(name))
+print(type(age))
+print(type(age1))
+print(type(a))
